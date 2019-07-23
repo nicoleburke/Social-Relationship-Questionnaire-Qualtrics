4d40d9871ca82be09011e601d9b5621b9a491d28
nicoleburke <nicole.b.burke@gmail.com>
2019-07-23 16:19:44 -0500

    making a relationship_perline function - complete!

--- relationship_perline_version4.R
+++ relationship_perline_version4.R
@@ -1,11 +1,23 @@
 #### Social Relationship Questionnaire from Qualtrics 
-#### 5.21.2019
+#### 7.23.2019
 #### script to make a dataframe with each relationships per line
 
 # create a function that separates out the dataframe 
-relationship_perline <- function(dataframe) {
+relationship_perline <- function(qualtricsoutput) {
   library(tidyverse)
-  newdataframe <- dataframe %>%
+  newdataframe <- qualtricsoutput %>%
+    rename(SubjectID = Participant....for.the.researcher.) %>%
+    group_by(SubjectID) %>%
+    summarize(Nodes = Please.select.all.the.people.who.your.child.sees.in.a.regular.week...Selected.Choice) %>%
+    mutate(Nodes = strsplit(as.character(Nodes), ",")) %>%
+    unnest(Nodes)
+  return(newdataframe)
+}
+
+# VERSION 3
+#relationship_perline <- function(qualtricsoutput) {
+  library(tidyverse)
+  newdataframe <- qualtricsoutput %>%
     group_by(Participant....for.the.researcher.) %>%
     summarize(Nodes = Please.select.all.the.people.who.your.child.sees.in.a.regular.week...Selected.Choice) %>%
     mutate(Nodes = strsplit(as.character(Nodes), ",")) %>%
@@ -16,3 +28,7 @@ relationship_perline <- function(dataframe) {
   newdataframe$Age <- NA
   return(newdataframe)
 }
+
+
+
+
