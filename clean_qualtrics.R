6a2d6e672ce3524bcc67272b4184cd14507c493b
nicoleburke <nicole.b.burke@gmail.com>
2019-07-25 12:44:09 -0500

    hiding the code that gets rid of colunms with all NAs - i'm worried that is going to mess up the logic

--- clean_qualtrics.R
+++ clean_qualtrics.R
@@ -4,7 +4,8 @@
 
 clean_qualtrics <- function(qualtricsoutput) {
   qualtricsoutput <- qualtricsoutput[order(qualtricsoutput$Participant....for.the.researcher.),]
-  qualtricsoutput <- qualtricsoutput[colSums(!is.na(qualtricsoutput)) > 0]
+  # hide this line of code for now - it will mess up the logic 
+  #qualtricsoutput <- qualtricsoutput[colSums(!is.na(qualtricsoutput)) > 0]
   library(tidyverse)
   newdataframe <- qualtricsoutput %>%
     rename(SubjectID = Participant....for.the.researcher.)
