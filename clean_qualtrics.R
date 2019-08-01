b3107cc248d9a21ff70841a5c6dc2336455f2de4
nicoleburke <nicole.b.burke@gmail.com>
2019-08-01 15:39:03 -0500

    adding a line to 'clean_qualtrics' that replaces NAs with 9999; this will allow the other functions to make boolean evaluations

--- clean_qualtrics.R
+++ clean_qualtrics.R
@@ -6,6 +6,8 @@ clean_qualtrics <- function(qualtricsoutput) {
   qualtricsoutput <- qualtricsoutput[order(qualtricsoutput$Participant....for.the.researcher.),]
   # hide this line of code for now - it will mess up the logic 
   #qualtricsoutput <- qualtricsoutput[colSums(!is.na(qualtricsoutput)) > 0]
+  # this replaces NAs with 9999
+  qualtricsoutput[is.na(qualtricsoutput)] <- 9999
   library(tidyverse)
   newdataframe <- qualtricsoutput %>%
     rename(SubjectID = Participant....for.the.researcher.)
