ed6250c5db6cb2b36692084781c05092a38ff36e
nicoleburke <nicole.b.burke@gmail.com>
2019-08-14 14:33:39 -0500

    I made a new section on the script to better document my work - i changed the recoded values in Qualtrics to fix the problem with some nodes

--- script to make package.R
+++ script to make package.R
@@ -22,16 +22,38 @@ test1 <- relationship_perline(qualdricsoutput_df)
 # this code delets cols that are NA
 df[colSums(!is.na(df)) > 0]
 trythis <- dummydata[colSums(!is.na(dummydata)) > 0]
-
 # this code will replace NAs 
 copy[is.na(copy)] <- 9999
 View(copy[,1:25])
+
 
 ### test the gender function
 test1 <- gender_perrelationship(qualdricsoutput_df, test1)
 
 ### where you're at: the script won't find stuff if you write it in...
 # if you write out a name for a node, it won't show up 
+
+
+
+###################### 8.14.19 ###################### 
+
+# i fixed something in the qualtrics survey - all the nodes should work now
+qualdricsoutput_df <- read.csv("Social+Relationship+Survey+Version+4_newexport.csv")
+
+# clean qualtrics survey
+qualdricsoutput_df$Participant....for.the.researcher.
+qualdricsoutput_df <- clean_qualtrics(qualdricsoutput_df)
+qualdricsoutput_df$SubjectID
+
+# make the relationshipdf and do gender function
+test1 <- relationship_perline(qualdricsoutput_df)
+test1 <- gender_perrelationship(qualdricsoutput_df, test1)
+
+
+
+
+
+
 
 
 
